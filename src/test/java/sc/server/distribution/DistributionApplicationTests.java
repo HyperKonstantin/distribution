2de35218ef478aeedeaad0d583163ac9d3aaee41
HyperKonstantin <kglembotskij@gmail.com>
2024-07-10 11:38:59 +0300

    init

---
+++ src/test/java/sc/server/distribution/DistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package sc.server.distribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
